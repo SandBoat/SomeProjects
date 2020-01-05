575b203ac641cae99947eff57b5f2110ae987107
SandBoat <695514503@qq.com>
2020-01-05 15:28:18 +0800

    [IMP] 完善 LeetCode 爬虫

--- leetcodePP/script/io.js
+++ leetcodePP/script/io.js
@@ -1,41 +1,91 @@
+/*
+ * @Description: file content
+ * @Author: ymt<mengtao.yan@hand-china.com>
+ * @Version: 1.0.0
+ * @Date: 2020-01-01 16:28:12
+ * @LastEditors  : ymt
+ * @LastEditTime : 2020-01-05 15:09:47
+ * @Copyright: Copyright (c) 2019, Hand
+ */
 const fs = require("fs");
 const path = require("path");
 
 const TEMPLATE_FULL_PATH = path.join(__dirname, "../template/README.md"); // 模板存放位置
 
+// 递归创建目录 异步方法
+const mkdirs = (dirname, callback) => {
+  fs.exists(dirname, function (exists) {
+    if (exists) {
+      callback();
+    } else {
+      // console.log(path.dirname(dirname));
+      mkdirs(path.dirname(dirname), function () {
+        fs.mkdir(dirname, callback);
+        console.log(
+          "在" + path.dirname(dirname) + "目录创建好" + dirname + "目录"
+        );
+      });
+    }
+  });
+};
+
+const delDir = path => {
+  let files = [];
+  if (fs.existsSync(path)) {
+    files = fs.readdirSync(path);
+    files.forEach((file, index) => {
+      let curPath = path + "/" + file;
+      if (fs.statSync(curPath).isDirectory()) {
+        delDir(curPath); //递归删除文件夹
+      } else {
+        fs.unlinkSync(curPath); //删除文件
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+
 // 预先读取文件并缓存
 let templateContent = "";
-const readTemplate = function() {
-  fs.readFile(TEMPLATE_FULL_PATH, function(err, data) {
+const readTemplate = function () {
+  fs.readFile(TEMPLATE_FULL_PATH, function (err, data) {
     if (err) return err;
 
     templateContent = data.toString();
   });
 };
 
-const write = function({ title, content }, path, filename, isAdd = false) {
+const contentWrite = function (data, path, filename, isAdd = false) {
   if (!path || !filename) throw new Error("no path or filename");
-  try {
-    const fullePath = path + filename;
-    if (!fs.existsSync(path)) {
-      fs.mkdirSync(path);
+
+  mkdirs(path, () => {
+    try {
+      const fullePath = path + filename;
+      writerStream = fs.createWriteStream(fullePath, {
+        flags: isAdd ? "a" : "w"
+      });
+      writerStream.write(data, "utf-8");
+      writerStream.end();
+    } catch (err) {
+      console.error(err);
     }
+  });
+};
 
-    const outputStr = templateContent
-      .replace("${title}", title)
-      .replace("${content}", content);
+const templateWrite = function ({ title, content }, path, filename, isAdd = false) {
+  if (!path || !filename) throw new Error("no path or filename");
 
-    writerStream = fs.createWriteStream(fullePath, {
-      flags: isAdd ? "a" : "w"
-    });
-    writerStream.write(outputStr, "utf-8");
-    writerStream.end();
-  } catch (err) {
-    console.error(err);
-  }
+  const outputStr = templateContent
+    .replace("${title}", title)
+    .replace("${content}", content);
+
+  contentWrite(outputStr, path, filename, isAdd);
 };
 
 module.exports = {
+  mkdirs,
+  delDir,
   readTemplate,
-  write
+  contentWrite,
+  templateWrite
 };
--- leetcodePP/server.js
+++ leetcodePP/server.js
@@ -4,32 +4,65 @@
  * @Version: 1.0.0
  * @Date: 2019-12-30 19:35:01
  * @LastEditors  : ymt
- * @LastEditTime : 2020-01-01 17:02:47
+ * @LastEditTime : 2020-01-05 15:26:32
  * @Copyright: Copyright (c) 2019, Hand
  */
-const { get } = require("lodash");
-const { readTemplate, write } = require("./script/io");
+const {
+  readTemplate,
+  contentWrite,
+  templateWrite,
+  delDir
+} = require("./script/io");
 const { getAllProblem, getProblemDetail } = require("./script/request");
+const _ = require("lodash");
 
 const DATA_PATH = "./result/";
+const DIFFICULTY_LEVEL = ["", "简单", "中等", "困难"];
 
 (async () => {
   // 初始化读取模板
   readTemplate();
+  // 清空目录
+  delDir(DATA_PATH);
+
   // 获取全部题目
   getAllProblem().then(all => {
-    all.slice(0, 10).forEach(({ title_slug, title, index }) => {
+    const list = [];
+    all.slice(100, 200).forEach(({ title_slug, title, index, difficulty }) => {
       getProblemDetail(title_slug).then(({ name, content }) => {
-        const fileName = `NO_${index}_${name}.md`;
+        const idx = `0000${index}`;
+        const fileName = `NO_${idx.slice(idx.length - 4)}_${name.replace(
+          " ",
+          ""
+        )}`;
+        const titleName = `${idx.slice(idx.length - 4)} ${name}`;
 
-        write(
+        // 生成详细 README
+        templateWrite(
           {
-            title: title,
+            title: name,
             content: content
           },
-          DATA_PATH,
-          fileName
+          `${DATA_PATH}/${fileName}/`,
+          "README.md"
         );
+
+        list.push({
+          key: index,
+          value: `| [${titleName}](./code/com/leetcode/solutions/${fileName}) | - | ${DIFFICULTY_LEVEL[difficulty]} |`
+        });
+
+        if (100 === list.length) {
+          // 生成列表 README
+          const data = _.sortBy(list, "key")
+            .map(item => item.value)
+            .join("\n");
+          contentWrite(
+            `| 题目&解法 | 时间复杂度 | 难易度 |\n| --- | --- | --- |\n` + data,
+            DATA_PATH,
+            "list.md"
+          );
+        }
       });
     });
   });
